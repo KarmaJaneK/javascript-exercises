c833ad0736e4f5b91dd69b52666ee00e1cce9252
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-08-03 17:01:34 +0700

    Exercise Passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeElement) {
+   
+    return array.reduce((acc,curr) => {
+        if (!removeElement.includes(curr)){
+            acc.push(curr);
+        }
+        return acc;
+    }, []);
 };
-
+console.log(removeFromArray([1, 2, 3, 4], [3]))
 // Do not edit below this line
 module.exports = removeFromArray;
