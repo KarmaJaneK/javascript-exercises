a8430cb5b8e705afcc17db90da9c2981c6edd180
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-08-03 12:47:54 +0700

    Exercise passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
 
+if(repeats < 0 || !Number.isInteger(repeats)){
+    return "ERROR";
+}
+    let result = "";
+   
+for (let i = 0; i < repeats; i++){
+result += string;}
+return result;
 };
 
+console.log(repeatString('hey', 3));
 // Do not edit below this line
 module.exports = repeatString;
