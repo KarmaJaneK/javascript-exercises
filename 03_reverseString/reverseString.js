6a4498bce806da2e906eca9f4d93e6c2f3d1ac41
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-08-03 15:58:04 +0700

    Exerise passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("")
+    return joinArray;
 };
 
+console.log(reverseString("Hello"));
 // Do not edit below this line
 module.exports = reverseString;
