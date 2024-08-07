2d5eac63f67f706d31c7c632a36a3c32f62cd108
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-08-07 12:40:15 +0700

    Exercise passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+//divisible by 4
+//not divisible by 100 unless..
+//divisible by 400
+if ((year % 4 == 0 && year % 100 !== 0) || (year % 400 == 0)){
+    return true;
+}else {
+    return false;
+}
+
 
 };
-
+console.log(leapYears)
 // Do not edit below this line
 module.exports = leapYears;
+ 
