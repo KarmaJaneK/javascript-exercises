bbdc382ecadbaf959bc6461acf84facdb08af261
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-08-08 10:28:27 +0700

    Exerise passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+    var tempC = ((tempF -32) * 5/9);
+    var tempCRounded = Math.round((tempC + Number.EPSILON)* 10) / 10;
+    return tempCRounded;
+   };
+
+const convertToFahrenheit = function(tempC) {
+  var tempF = ((tempC * 9/5) + 32);
+  var tempFRounded = Math.round((tempF + Number.EPSILON)* 10) / 10;
+  return tempFRounded;
 };
 
-const convertToFahrenheit = function() {
-};
-
+console.log(convertToCelsius());
+console.log(convertToFahrenheit());
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
