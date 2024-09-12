65e7c893c4d57a09d03bb44eed86a06305ba7ecb
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-09-12 14:38:12 +0700

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,24 +3,33 @@ return a + b;
 	
 };
 
-const subtract = function() {
-
+const subtract = function(a,b) {
+return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 
 };
 
-const power = function() {
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);  
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0 || n === 1) return 1;
+  let result = 1;
+  for (let i = n; i > 1; i--){
+    result *= i;
+  }
+  return result;
 	
 };
 
